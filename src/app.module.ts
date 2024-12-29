100f8828fa2fa8ac31b0a90f2d3f281a31c2103f
DanielF-Cardoso <danielfca122@gmail.com>
2024-12-29 19:58:30 -0300

    feat: implement create account controller and configure ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { PrismaService } from './prisma/prisma.service'
+import { ConfigModule } from '@nestjs/config'
+import { CreateAccountController } from './controllers/create-account.controller'
+import { envSchema } from './envt'
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
+  controllers: [CreateAccountController],
+  providers: [PrismaService],
 })
 export class AppModule {}
