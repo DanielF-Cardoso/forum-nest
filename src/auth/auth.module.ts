b38abde5f9594aa14f187c8fe73c12383f0d1595
DanielF-Cardoso <danielfca122@gmail.com>
2024-12-30 00:04:24 -0300

    feat: add JWT Auth Guard and create CurrentUser decorator

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
 import { Env } from 'src/envt'
+import { JwtStrategy } from './jwt.strategy'
 
 @Module({
   imports: [
@@ -22,5 +23,6 @@ import { Env } from 'src/envt'
       },
     }),
   ],
+  providers: [JwtStrategy],
 })
 export class AuthModule {}
