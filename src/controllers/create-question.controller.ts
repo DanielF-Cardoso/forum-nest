81d3c2cab253e587c803f923c864c8ae4009d33e
DanielF-Cardoso <danielfca122@gmail.com>
2025-01-01 11:18:53 -0300

    chore: configure Vitest for testing

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,9 +1,9 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common'
-import { CurrentUser } from 'src/auth/current-user-decorator'
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
-import { UserPayload } from 'src/auth/jwt.strategy'
-import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
-import { PrismaService } from 'src/prisma/prisma.service'
+import { CurrentUser } from '@//auth/current-user-decorator'
+import { JwtAuthGuard } from '@//auth/jwt-auth.guard'
+import { UserPayload } from '@//auth/jwt.strategy'
+import { ZodValidationPipe } from '@//pipes/zod-validation-pipe'
+import { PrismaService } from '@//prisma/prisma.service'
 import { z } from 'zod'
 
 const createQuestionBodySchema = z.object({
@@ -18,7 +18,7 @@ type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   @Post()
   async handle(
