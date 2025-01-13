0636d3e58ed9dcd13fa659687db8e66e25a1fcf7
DanielF-Cardoso <danielfca122@gmail.com>
2025-01-12 23:32:03 -0300

    feat(cache): implement Redis-based caching and repository integration

--- src/infra/cache/redis/cache.module.ts
+++ src/infra/cache/redis/cache.module.ts
@@ -1,9 +1,18 @@
 import { EnvModule } from '@/infra/env/env.module'
-import { EnvService } from '@/infra/env/env.service'
 import { Module } from '@nestjs/common'
+import { CacheRepository } from '../cache-repository'
+import { RedisCacheRepository } from './redis-cache-repository'
+import { RedisService } from './redis-service'
 
 @Module({
   imports: [EnvModule],
-  providers: [EnvService],
+  providers: [
+    RedisService,
+    {
+      provide: CacheRepository,
+      useClass: RedisCacheRepository,
+    },
+  ],
+  exports: [CacheRepository],
 })
 export class CacheModule {}
